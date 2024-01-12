7aba03033267117b3b17fb5afe11897d37212fb8
jessica-sobreira <j.jecristina@gmail.com>
2024-01-12 16:09:05 -0300

    index e cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
+import cors from "cors";
+
 import { AlunoController } from "./controllers/aluno.controller";
 import { AvaliacaoController } from "./controllers/avaliacao.controller";
+import { AuthController } from "./controllers/auth.controller";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const alunoController = new AlunoController();
 const avaliacaoController = new AvaliacaoController(); 
+const authController = new AuthController();
 
 //criar novo aluno
 app.post("/aluno", alunoController.criarAluno);
@@ -21,10 +26,18 @@ app.delete("/aluno/:id", alunoController.deletarAluno);
 //atualizar aluno
 app.put("/aluno/:id", alunoController.atualizarAluno);
 
-//Rotas de Avaliação
 
 //criar avaliação
 app.post("/aluno/:id/avaliacao", avaliacaoController.criarAvaliacao);
+
+//listar avaliações
+app.get("/aluno/:id/avaliacao", avaliacaoController.listarAvaliacoes);
+
+//atualizar avaliação
+app.put("/aluno/:id/avaliacao/:idAvaliacao", avaliacaoController.atualizarAvaliacao);
+
+//autenticar
+app.post("/login", authController.login);
 
 
 app.listen(3008, () => {
